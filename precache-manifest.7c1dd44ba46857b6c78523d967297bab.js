a6de15dedecc9058a37bad0c14786aa62588196d
niloofarf <fathi.niloofar@gmail.com>
2020-10-17 21:27:06 +0330

    Updates

---
+++ precache-manifest.7c1dd44ba46857b6c78523d967297bab.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60417b9c5bcb886056105bd370c75bc",
+    "url": "/online-pizza/index.html"
+  },
+  {
+    "revision": "33429b018f9db9e94faa",
+    "url": "/online-pizza/static/css/2.7844fb40.chunk.css"
+  },
+  {
+    "revision": "46a02d7036289f0a4e43",
+    "url": "/online-pizza/static/css/main.e6dcaf00.chunk.css"
+  },
+  {
+    "revision": "33429b018f9db9e94faa",
+    "url": "/online-pizza/static/js/2.7c5c830a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/online-pizza/static/js/2.7c5c830a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46a02d7036289f0a4e43",
+    "url": "/online-pizza/static/js/main.eece05cf.chunk.js"
+  },
+  {
+    "revision": "7e110b639fcf27c1a301",
+    "url": "/online-pizza/static/js/runtime-main.16fb28a5.js"
+  },
+  {
+    "revision": "385001fd4e1807f9a3d55d2697d0c994",
+    "url": "/online-pizza/static/media/Vazir.385001fd.eot"
+  },
+  {
+    "revision": "5c7abcf135f2445506d83594d50db435",
+    "url": "/online-pizza/static/media/Vazir.5c7abcf1.woff"
+  },
+  {
+    "revision": "bed047dac8bc9d762b174a96f09f4837",
+    "url": "/online-pizza/static/media/Vazir.bed047da.ttf"
+  },
+  {
+    "revision": "db055c8873d217d4a8b02f13b3618de1",
+    "url": "/online-pizza/static/media/Vazir.db055c88.woff2"
+  },
+  {
+    "revision": "f69a7fd927b883d6a60d4e491bc5a97a",
+    "url": "/online-pizza/static/media/bg.f69a7fd9.jpg"
+  },
+  {
+    "revision": "c231e9a1d1a34b5802f0051ed659e265",
+    "url": "/online-pizza/static/media/footerbg.c231e9a1.jpg"
+  },
+  {
+    "revision": "06e65902bc9e55e1dd87dae9030e1753",
+    "url": "/online-pizza/static/media/logo.06e65902.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/online-pizza/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/online-pizza/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/online-pizza/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/online-pizza/static/media/slick.f97e3bbf.svg"
+  }
+]);
